c1b65cc9b6bf641576326f4d7d5f3cbeab95d299
krmsan <sankrmsan@gmail.com>
2022-11-28 20:21:50 +0300

    switch

--- src/sorular_6_SwitchStatements/S04_ISTQBacilimi.java
+++ src/sorular_6_SwitchStatements/S04_ISTQBacilimi.java
@@ -1,10 +1,35 @@
 package sorular_6_SwitchStatements;
 
-public class S03_ISTQBacilimi {
+import sorular_0ScannerCagirma.Scan;
 
-    //Soru 4- Kullanicidan ISTQB kisaltmasindan harfin anlamini ogrenmek istedigini alin
+public class S04_ISTQBacilimi extends Scan {
+
+    //Soru 4- Kullanicidan ISTQB kisaltmasindan harfin anlamini ogrenmek istediginizi alin
     //ve girilen harfin karsiligini yazdirin.
+    // I : International S : Software T : Testing Q : Qualifications B: Board
     public static void main(String[] args) {
+
+        Scan obj=new Scan();
+
+        switch (obj.harfAl()) {
+            case 'I':
+            case 'i': System.out.println("International");
+                break;
+            case 'S':
+            case 's': System.out.println("Software");
+                break;
+            case 'T':
+            case 't': System.out.println("Testing");
+                break;
+            case 'Q':
+            case 'q': System.out.println("Qualifications");
+                break;
+            case 'B':
+            case 'b': System.out.println("Board");
+                break;
+            default: System.out.println("gecersiz harf girdiniz");
+
+        }
 
     }
 }
