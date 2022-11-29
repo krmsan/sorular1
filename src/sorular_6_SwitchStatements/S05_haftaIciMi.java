26108be8a804d407fe5be93371fef3203b545142
krmsan <sankrmsan@gmail.com>
2022-11-29 14:50:08 +0300

    switch

--- src/sorular_6_SwitchStatements/S05_haftaIciMi.java
+++ src/sorular_6_SwitchStatements/S05_haftaIciMi.java
@@ -1,2 +1,19 @@
-package sorular_6_SwitchStatements;public class S05_haftaIciMi {
+package sorular_6_SwitchStatements;
+
+public class S05_haftaIciMi {
+    //Soru 5- Kullanicidan gun numarasini alip hafta ici veya hafta sonu yazdirin
+    public static void main(String[] args) {
+        int i = 6;
+        String[] hafta = {"", "pazartesi", "sali", "carsamba", "persembe", "cuma", "cumartesi", "pazar"};
+        switch (i) {
+            case 1: case 2: case 3: case 4: case 5:
+                System.out.println(hafta[i]+" hafta icidir");
+                break;
+            case 6: case 7:
+                System.out.println(hafta[i]+" hafta sonudur");
+                break;
+            default:
+                System.out.println("yanlıs giriş");
+        }
+    }
 }
