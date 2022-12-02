73e363cd25b5102e66e2d8a2b709b262083f0597
krmsan <sankrmsan@gmail.com>
2022-12-02 20:39:24 +0300

    ternary

--- src/sorular_5_ternary/S04_BuyukSayi.java
+++ src/sorular_5_ternary/S04_BuyukSayi.java
@@ -1,4 +1,14 @@
 package sorular_5_ternary;
 
-public class S04_BuyukSayi {
+import sorular_0ScannerCagirma.Scan;
+
+public class S04_BuyukSayi extends Scan {
+    //iki sayi alin ve buyuk olmayni yazdirin
+    public static void main(String[] args) {
+        S04_BuyukSayi obj = new S04_BuyukSayi();
+        int sayi1 = obj.sayiAl();
+        int sayi2 = obj.sayiAl();
+        System.out.println(sayi1 > sayi2 ? sayi1 : sayi2);
+
+    }
 }
--- src/sorular_5_ternary/S05_mutlakDeger.java
+++ src/sorular_5_ternary/S05_mutlakDeger.java
@@ -1,2 +1,12 @@
-package sorular_5_ternary;public class S05_mutlakDeger {
+package sorular_5_ternary;
+
+public class S05_mutlakDeger {
+    //Soru:5 bir sayi alin ve mutlak degerini yazdirin
+
+    public static void main(String[] args) {
+        int a = -3;
+        System.out.println(a < 0 ? a * -1 : a);
+
+
+    }
 }
