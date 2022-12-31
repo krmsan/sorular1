63fb466a15f799c4dc4a1871b801acb391fb667e
krmsan <sankrmsan@gmail.com>
2022-12-31 23:16:27 +0300

    rakam toplami

--- src/sorular_1_valueof_parseInt/S022_RT_Math.java
+++ src/sorular_1_valueof_parseInt/S022_RT_Math.java
@@ -12,12 +12,38 @@ public class S022_RT_Math {
             for (int j = 0; j < sayistr.length(); j++) {
                 sayilar[j] += sayi % 10;
                 sayi = sayi / 10;
+
             }
             top += sayilar[i];
         }
         System.out.println(top);
 
-        //========================
+        //=======================YADA===============
+         sayi = 1234567;
+        top = 0;
+         sayistr = "" + sayi;
+        sayilar = new int[sayistr.length()];
+
+            for (int j = 0; j < sayistr.length(); j++) {
+                sayilar[j] += sayi % 10;
+                sayi = sayi / 10;
+                top += sayilar[j];
+            }
+        System.out.println(top);
+        //=======================YADA===============
+
+
+        sayi = 1234567;
+        top = 0;
+        sayistr = "" + sayi;
+        for (int j = 0; j < sayistr.length(); j++) {
+            top+= sayi % 10;
+            sayi = sayi / 10;
+        }
+        System.out.println(top);
+        //=======================YADA===============
+
+
 
     }
 }
